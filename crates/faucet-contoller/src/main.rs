6441c1a65021db8b564d87b50fe6c07a72eb44cd
Dmitry Lavrenov <lawrenowdima@gmail.com>
2024-10-19 15:06:18 +0300

    Define args for faucet controller to be run

--- crates/faucet-contoller/src/main.rs
+++ crates/faucet-contoller/src/main.rs
@@ -1,8 +1,32 @@
 //! Faucet controller entrypoint.
 
+use clap::Parser;
+
+/// Args to run the faucet controller.
+#[derive(Parser, Debug)]
+#[command(about)]
+struct Args {
+    /// Name of the faucet controller.
+    #[arg(long)]
+    name: String,
+
+    /// Path to the data folder.
+    #[arg(long)]
+    data_path: String,
+
+    /// Url to the main server.
+    #[arg(long)]
+    main_server_url: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    println!("I'm faucet!");
+    let args = Args::parse();
+
+    println!("faucet controller is run ");
+    println!("Name:\t{}", args.name);
+    println!("Data Path:\t{}", args.data_path);
+    println!("Main Server Utl:\t{}", args.main_server_url);
 
     Ok(())
 }
